6469d6bd64e9eb270d666f16590e07b305eb8124
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2023-11-28 16:32:08 +0900

    components 생성

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Bottombar() {
+  return <h1>Bottom</h1>;
+}
+
+export default Bottombar;
---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LeftSidebar() {
+  return <h1>Left</h1>;
+}
+
+export default LeftSidebar;
